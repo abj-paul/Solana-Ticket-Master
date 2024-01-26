854634e402e36ab1af09ce4c6d3a21f8c284148a
Abhijit Paul <bsse1201@iit.du.ac.bd>
2024-01-26 21:57:06 +0600

    Added compressed NFT

--- cmv3/cm-umi/app.ts
+++ cmv3/cm-umi/app.ts
@@ -9,29 +9,14 @@ import {
 const QUICKNODE_RPC = 'https://solana-devnet.g.alchemy.com/v2/MinrZVld3RStLg4EBIFTfi9N7dLADMwU'; // 👈 Replace with your QuickNode Solana Devnet HTTP Endpoint
 
 
-import { percentAmount, generateSigner, some } from "@metaplex-foundation/umi";
-import { TokenStandard } from "@metaplex-foundation/mpl-token-metadata";
-
-const creatorA = generateSigner(umi).publicKey;
-const creatorB = generateSigner(umi).publicKey;
-const candyMachineSettings = {
-  tokenStandard: TokenStandard.NonFungible,
-  sellerFeeBasisPoints: percentAmount(33.3, 2),
-  symbol: "MYPROJECT",
-  maxEditionSupply: 0,
-  isMutable: true,
-  creators: [
-    { address: creatorA, percentageShare: 50, verified: false },
-    { address: creatorB, percentageShare: 50, verified: false },
-  ],
-};
-
 async function connectToCandyMachine(pubKey){
     const umi = createUmi(QUICKNODE_RPC).use(mplCandyMachine());
     const candyMachinePublicKey = publicKey(pubKey);
     const candyMachine = await fetchCandyMachine(umi, candyMachinePublicKey);
     const candyGuard = await fetchCandyGuard(umi, candyMachine.mintAuthority);
     console.log(`✅ - Connected to Candy Machine ${pubKey}`);
+
+    console.log(candyGuard.guards); // All guard settings.
 }
 connectToCandyMachine("G4suGCZ3PokhsdL15Jyru22aeFBExKxmJLvaPsTRFEoh")
 .then((data)=>{})
