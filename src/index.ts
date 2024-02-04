410ec4e30bb6f46e55061971ee4399c8267aa797
Tahlil <bsse0803@iit.du.ac.bd>
2024-02-05 01:24:39 +0600

    Resolve cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { testEndpoint, mintTicketEndpoint, getNFTEndpoint, verifyNFTEndpoint } from './controller';
 import bodyParser from 'body-parser';
-
+import cors from 'cors';
 const app = express();
 app.use(bodyParser.json());
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
 const port = 3001;
+app.use(cors(options));
 
 // API endpoint for a simple test
 app.get('/server', testEndpoint);
