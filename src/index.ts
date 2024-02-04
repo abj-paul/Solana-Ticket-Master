7789a0a1e970312ed941da24680539d56261640a
Tahlil <bsse0803@iit.du.ac.bd>
2024-02-04 13:10:28 +0600

    Add route to mint token

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
-import { testEndpoint } from './controller';
+import { testEndpoint, mintTicketEndpoint } from './controller';
 
 const app = express();
 const port = 3001;
 
 // API endpoint for a simple test
 app.get('/server', testEndpoint);
+app.get('/mint', mintTicketEndpoint);
 
 // Start the server
 app.listen(port, () => {
